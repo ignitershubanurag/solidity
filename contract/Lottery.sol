e8a98d458059eb7f28353631a3203278ca31a8f3
"Anurag" <"ignitershub.anurag@gmail.com">
2022-06-15 10:59:21 +0530

    lottery commit

---
+++ contract/Lottery.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >0.8.11;
+contract Lottery {
+    string private name;
+    address public owner;
+    address payable[] public participant;
+    constructor(string memory _name){
+        name = _name;
+        owner = msg.sender;
+    }
+    modifier checkOwner(){
+        require(owner == msg.sender, "Only owner can access");
+        _;
+    }
+    modifier canParticipate(){
+        require(owner != msg.sender, "Only owner can access");
+        _;
+    }
+    receive() external payable canParticipate{
+        participant.push(payable(msg.sender));
+    }
+
+    function display() view public checkOwner returns(string memory, address){
+        return (name, owner);
+    }
+
+    function participate() public canParticipate view returns(address payable[] memory){
+        return participant;
+    }
+
+    function balance() public checkOwner view returns(uint){
+        return address(this).balance;
+    }
+}
