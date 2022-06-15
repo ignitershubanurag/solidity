83f0f4cf8017e090d3bbf9ca0944df7520c73c80
"Anurag" <"ignitershub.anurag@gmail.com">
2022-06-15 11:45:56 +0530

    lotter1 commit v1.0

--- contract/Lottery.sol
+++ contract/Lottery.sol
@@ -14,7 +14,7 @@ contract Lottery {
         _;
     }
     modifier canParticipate(){
-        require(owner != msg.sender, "Only owner can access");
+        require(owner != msg.sender, "Owner can not participate");
         _;
     }
     receive() external payable canParticipate{
@@ -32,5 +32,16 @@ contract Lottery {
 
     function balance() public checkOwner view returns(uint){
         return address(this).balance;
+    }
+    function random() public view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, participant.length)));
+    }
+
+    function selectWinner() public checkOwner {
+        require(participant.length > 3);
+        uint num = random()%participant.length;
+        address payable winner = participant[num];
+        winner.transfer(balance());
+        participant = new address payable[](0);
     }
 }
