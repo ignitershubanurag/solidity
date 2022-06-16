be266288bf371268d60701aa255678491cbec64e
"Anurag" <"ignitershub.anurag@gmail.com">
2022-06-16 09:03:25 +0530

    Croudfunding 1

---
+++ contract/CrowdFunding.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >0.8.11;
+/*
+User can transfer fund to contract
+There should be limit for amount and time
+if fund not collected within time then all fund should be returned
+
+
+*/
+contract CrowdFunding {
+}
